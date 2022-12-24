c2d986c44d3e34351ea2e1b7b4c3dc6daf8e2732
Gabriel Xavier <xavierdesigner28@gmail.com>
2022-12-24 18:45:57 -0300

    Consumindo uma API

---
+++ script.js
@@ -0,0 +1,26 @@
+const api = "https://digimon-api.vercel.app/api/digimon";
+
+
+window.onload = function(){
+
+    var filter = "Champion"
+
+    fetch(api)
+        .then(data => data.json())
+        .then(data => {
+            for (let index = 0; index < data.length; index++) {
+                
+                var digimon = `
+                <div id="digimon">
+                    <img src="${data[index].img}">
+                    <p>${data[index].level}</p>
+                    <p>${data[index].name}</p>
+                </div>
+                `
+                
+                if (data[index].level == filter) {
+                    document.getElementById("digimons").innerHTML += digimon;
+                }
+            }
+        })
+}
